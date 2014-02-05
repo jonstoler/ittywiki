0d9141014af48abdaaa9669fd90271343da19b8c
Jonathan Stoler <jrstoler@Jonathans-MacBook-Pro.local>
2014-02-04 20:14:06 -0800

    version 1.0.1

--- core/defaults.php
+++ core/defaults.php
@@ -7,6 +7,6 @@
 	g::set('wiki.dir', '/wiki');
 
 	g::set('ittywiki.download.url', 'https://github.com/jonstoler/ittywiki');
-	g::set('ittywiki.version.string', '1.0');
-	g::set('ittywiki.version', 1.0);
+	g::set('ittywiki.version.string', '1.0.1');
+	g::set('ittywiki.version', 1.01);
 ?>
